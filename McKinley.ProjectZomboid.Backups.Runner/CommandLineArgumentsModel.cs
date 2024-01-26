1c34db874251a37aefe5913b500d15f63f596c57
Jon McKinley <jonpmck@gmail.com>
2024-01-26 01:21:59 -0500

    adding better descriptions

--- McKinley.ProjectZomboid.Backups.Runner/CommandLineArgumentsModel.cs
+++ McKinley.ProjectZomboid.Backups.Runner/CommandLineArgumentsModel.cs
@@ -6,22 +6,22 @@ namespace McKinley.ProjectZomboid.Backups.Runner;
 
 public class CommandLineArgumentsModel
 {
-    [Option('t', "type", HelpText = "Backup file type. Zip and TarZLib are supported.")]
+    [Option('t', "type", HelpText = "The backup file type. Zip and TarZLib are supported. The default is Zip")]
     public BackupType BackupType { get; set; } = BackupType.Zip;
 
-    [Option('o', "output", HelpText = "Backup output folder. By default the current directory will be used.")]
+    [Option('o', "output", HelpText = "Backup output folder. By default, the current directory will be used.")]
     public string OutputFolder { get; set; } = Environment.CurrentDirectory;
 
-    [Option('s', "save-directory", HelpText = "Project Zomboid Saves Directory. If not provided, it will try to detect your default Project Zomboid saves directory (ex: %USERPROFILE%\\Zomboid\\Saves\\Sandbox).")]
+    [Option('s', "save-directory", HelpText = "Project Zomboid Saves Directory. By default, it will try to detect your default Project Zomboid saves directory (ex: %USERPROFILE%\\Zomboid\\Saves\\Sandbox).")]
     public string SaveDirectory { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Zomboid\Saves\Sandbox");
 
-    [Option("save-name", HelpText = "Only back up this save, ignores all others.")]
+    [Option("save-name", HelpText = "Only back up this save, ignores all others. There is no default value.")]
     public string? SaveName { get; set; }
 
-    [Option("log-level", HelpText = "The log level to output to the console.")]
+    [Option("log-level", HelpText = "The log level to output to the console (Trace/Debug/Information/Warning/Error/Critical/None). The default is Information.")]
     public LogLevel LogLevel { get; set; } = LogLevel.Information;
 
-    [Option("zip-filename", HelpText = "Zip backup file name")]
+    [Option("zip-filename", HelpText = "Zip backup file name. The default is ProjectZomboid-Backups.zip")]
     public string ZipFileName { get; set; } = "ProjectZomboid-Backups.zip";
 
     [Option("compression-level", HelpText = "Compression Level (Optimal/Fastest/NoCompression/SmallestSize). The default is Optimal.")]
